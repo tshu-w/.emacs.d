8d056263c553f79a825cc280f3f3f8b30ddb3565
Tianshu Wang <volekingsg@gmail.com>
2020-03-04 10:54:26 +0800

    Initial commit: my Spacemacs

---
+++ lisp/editor-projectile.el
@@ -0,0 +1,72 @@
+;;; editor-projectile.el -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  Tianshu Wang
+
+;; Author: Tianshu Wang <volekingsg@gmail.com>
+
+(use-package projectile
+  :init
+  (setq projectile-sort-order 'recentf
+        projectile-completion-system 'ivy
+        projectile-cache-file (concat cache-dir "projectile.cache")
+        projectile-known-projects-file (concat cache-dir "projectile-bookmarks.eld"))
+  (projectile-mode)
+  :general
+  (tyrant-def
+    "p"  '(:ignore t :which-key "projects")
+    "p!" 'projectile-run-shell-command-in-root
+    "p&" 'projectile-run-async-shell-command-in-root
+    "p%" 'projectile-replace-regexp
+    "pa" 'projectile-toggle-between-implementation-and-test
+    "pb" 'projectile-switch-to-buffer
+    "pc" 'projectile-compile-project
+    "pd" 'projectile-find-dir
+    "pD" 'projectile-dired
+    "pe" 'projectile-edit-dir-locals
+    "pf" 'projectile-find-file
+    "pF" 'projectile-find-file-dwim
+    "pg" 'projectile-find-tag
+    "pG" 'projectile-regenerate-tags
+    "pI" 'projectile-invalidate-cache
+    "pk" 'projectile-kill-buffers
+    "pp" 'projectile-switch-project
+    "pr" 'projectile-recentf
+    "pR" 'projectile-replace
+    "pT" 'projectile-test-project
+    "pv" 'projectile-vc))
+
+(use-package counsel-projectile
+  :config
+  (defun counsel-projectile-rg-region-or-symbol ()
+    "Use `counsel-projectile-rg' to search for
+    the selected region or the symbol around point in the current
+    project ."
+    (interactive)
+    (let ((counsel-projectile-rg-initial-input
+           '(if (region-active-p)
+                (buffer-substring-no-properties
+                 (region-beginning) (region-end))
+              (thing-at-point 'symbol t))))
+      (counsel-projectile-rg)))
+
+  (ivy-add-actions 'counsel-projectile-find-file
+                   '(("R" (lambda (arg)
+                            (interactive)
+                            (call-interactively
+                             #'projectile-invalidate-cache)
+                            (ivy-resume)) "refresh list")))
+  :general
+  (tyrant-def
+    "/"     '(counsel-projectile-rg :which-key "search project")
+    "*"     '(counsel-projectile-rg-region-or-symbol :which-key "search project w/input")
+    "p SPC" 'counsel-projectile
+    "pb"    'counsel-projectile-switch-to-buffer
+    "pd"    'counsel-projectile-find-dir
+    "pp"    'counsel-projectile-switch-project
+    "pf"    'counsel-projectile-find-file
+    "p!"    'projectile-run
+    "sp"    'counsel-projectile-rg
+    "sP"    'counsel-projectile-rg-region-or-symbol))
+
+
+(provide 'editor-projectile)
---
+++ lisp/editor-vsc.el
@@ -0,0 +1,207 @@
+;;; editor-vsc.el -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  Tianshu Wang
+
+;; Author: Tianshu Wang <volekingsg@gmail.com>
+
+
+(use-package magit
+  :config
+  (setq magit-revision-show-gravatars '("^Author:     " . "^Commit:     ")
+        magit-display-buffer-function 'magit-display-buffer-fullframe-status-v1
+        magit-save-repository-buffers 'dontask)
+
+  (defun org-reveal-advice (&rest _args)
+    "Unfold the org headings for a target line.
+This can be used to advice functions that might open .org files.
+
+For example: To unfold from a magit diff buffer, evaluate the following:
+(advice-add 'magit-diff-visit-file :after #'org-reveal-advice)"
+         (when (derived-mode-p 'org-mode)
+           (org-reveal)))
+
+  (advice-add 'magit-blame-addition           :after #'org-reveal-advice)
+  (advice-add 'magit-diff-visit-file          :after #'org-reveal-advice)
+  (advice-add 'magit-diff-visit-worktree-file :after #'org-reveal-advice)
+
+  (defhydra git-blame-menu (
+                            :pre (unless (bound-and-true-p magit-blame-mode)
+                                   (call-interactively 'magit-blame-addition))
+                            :foreign-keys run
+                            :hint nil)
+    "
+  Git Blame Transient State
+  Press [_b_] again to blame further in the history, [_q_] to go up or quit."
+    ("b" magit-blame-addition)
+    ("q" magit-blame-quit :exit (bound-and-true-p magit-blame-mode)))
+
+  (general-define-key :keymaps '(magit-blame-read-only-mode-map)
+                      :states  '(normal)
+                      "RET"    'magit-show-commit)
+
+  (despot-def with-editor-mode-map
+    ","      'with-editor-finish
+    "a"      'with-editor-cancel
+    "c"      'with-editor-finish
+    "k"      'with-editor-cancel)
+  (despot-def magit-log-select-mode-map
+    ","      'magit-log-select-pick
+    "a"      'magit-log-select-quit
+    "c"      'magit-log-select-pick
+    "k"      'magit-log-select-quit)
+  :general
+  (tyrant-def
+    "g"   '(:ignore t :which-key "git")
+    "gs"  'magit-status
+    "gf"  '(:ignore t :which-key "file")
+    "gb"  'git-blame-menu/body
+    "gc"  'magit-clone
+    "gfF" 'magit-find-file
+    "gfl" 'magit-log-buffer-file
+    "gfd" 'magit-diff
+    "gi"  'magit-init
+    "gL"  'magit-list-repositories
+    "gm"  'magit-dispatch
+    "gs"  'magit-status
+    "gS"  'magit-stage-file
+    "gU"  'magit-unstage-file))
+
+(use-package evil-magit
+  :hook (magit-mode . evil-magit-init))
+
+(use-package magit-gitflow
+  :disabled t
+  :hook (magit-mode . turn-on-magit-gitflow)
+  :config (define-key magit-mode-map "%" 'magit-gitflow-popup))
+
+(use-package magit-svn
+  :disabled t
+  :hook (magit-mode . turn-on-magit-svn)
+  :config (define-key magit-mode-map "~" 'magit-svn))
+
+(use-package forge
+  :after magit
+  :config
+  (setq forge-database-file (concat cache-dir "forge-database.sqlite"))
+  (despot-def forge-topic-mode-map
+    "c" 'forge-create-post
+    "e" 'forge-edit-post)
+  (despot-def forge-post-mode-map
+    "," 'forge-post-submit
+    "c" 'forge-post-submit
+    "k" 'forge-post-cancel
+    "a" 'forge-post-cancel))
+
+(use-package transient
+  :config
+  (setq transient-levels-file (expand-file-name "transient/levels.el" cache-dir)
+        transient-values-file (expand-file-name "transient/values.el" cache-dir)
+        transient-history-file (expand-file-name "transient/history.el" cache-dir))
+  (transient-bind-q-to-quit))
+
+(use-package browse-at-remote
+  :general (tyrant-def "go" 'browse-at-remote))
+
+(use-package diff-hl
+  :init
+  (setq diff-hl-side 'right)
+  (global-diff-hl-mode)
+  (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
+  :general
+  (tyrant-def "g=" 'diff-hl-diff-goto-hunk)
+  (general-define-key :states 'normal
+                      "[ h" 'diff-hl-previous-hunk
+                      "] h" 'diff-hl-next-hunk))
+
+(use-package git-timemachine
+  :config
+  (defhydra git-timemachine-menu (
+                                  :pre (unless (bound-and-true-p git-timemachine-mode)
+                                         (call-interactively 'git-timemachine))
+                                  :post (when (bound-and-true-p git-timemachine-mode)
+                                          (git-timemachine-quit))
+                                  :hint nil)
+    "
+Git Timemachine Transient State
+[_p_/_N_] previous [_n_] next [_c_] current [_g_] goto nth rev [_Y_] copy hash [_q_] quit"
+    ("c" git-timemachine-show-current-revision)
+    ("g" git-timemachine-show-nth-revision)
+    ("p" git-timemachine-show-previous-revision)
+    ("n" git-timemachine-show-next-revision)
+    ("N" git-timemachine-show-previous-revision)
+    ("Y" git-timemachine-kill-revision)
+    ("q" nil))
+  :general
+  (tyrant-def "gt" 'git-timemachine-menu/body))
+
+(use-package git-link
+  :config
+  (setq git-link-open-in-browser t)
+
+  (defun git-link-copy-url-only ()
+    "Only copy the generated link to the kill ring."
+    (interactive)
+    (let (git-link-open-in-browser)
+      (call-interactively 'git-link)))
+
+  (defun git-link-commit-copy-url-only ()
+    "Only copy the generated link to the kill ring."
+    (interactive)
+    (let (git-link-open-in-browser)
+      (call-interactively 'git-link-commit)))
+  :general
+  (tyrant-def
+    "gl"  '(:ignore t :which-key "links")
+    "glc" 'git-link-commit
+    "glC" 'git-link-commit-copy-url-only
+    "gll" 'git-link
+    "glL" 'git-link-copy-url-only
+    "glh" 'git-link-homepage))
+
+(use-package git-messenger
+  :general (tyrant-def "gM" 'git-messenger:popup-message))
+
+(use-package gitignore-templates
+  :general
+  (tyrant-def "gI" 'gitignore-templates-new-file)
+  (despot-def gitignore-mode-map
+    "i" 'gitignore-templates-insert))
+
+(use-package gitattributes-mode)
+
+(use-package gitconfig-mode)
+
+(use-package gitignore-mode)
+
+(use-package gist
+  :config
+  (evilified-state-evilify-map gist-list-menu-mode-map
+                               :mode gist-list-mode
+                               :bindings
+                               "f" 'gist-fetch-current
+                               "K" 'gist-kill-current
+                               "o" 'gist-browse-current-url)
+  (evilified-state-evilify-map gist-list-mode-map
+                               :mode gist-list-mode
+                               :bindings
+                               (kbd "gr") 'gist-list-reload)
+  :general
+  (tyrant-def
+    "gg"  '(:ignore t :which-key "github gist")
+    "ggb" 'gist-buffer
+    "ggB" 'gist-buffer-private
+    "ggl" 'gist-list
+    "ggr" 'gist-region
+    "ggR" 'gist-region-private))
+
+(use-package helm)
+
+(use-package helm-github-stars
+  :commands (helm-github-stars)
+  :config
+  (setq helm-github-stars-username "tshu-w"))
+
+(use-package orgit)
+
+
+(provide 'editor-vsc)
