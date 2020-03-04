eb686eeb5ffa46dd3c8695ddbbff908f6dd7345c
wangtianshu <volekingsg@gmail.com>
2020-03-04 15:23:11 +0800

    add some basic programming language libraries

---
+++ .custom.el
@@ -0,0 +1,36 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(helm-github-stars-refetch-time 1 t)
+ '(helm-github-stars-username "tshu-w" t)
+ '(ivy-posframe-display-functions-alist
+   (quote
+    ((swiper)
+     (complete-symbol . ivy-posframe-display-at-point)
+     (t . ivy-posframe-display-at-frame-center))) t)
+ '(ivy-posframe-parameters (quote ((left-fringe . 8) (right-fringe . 8))) t)
+ '(ivy-posframe-width 120 t)
+ '(ivy-rich-path-style (quote abbrev))
+ '(ivy-virtual-abbreviate (quote full))
+ '(package-selected-packages
+   (quote
+    (yaml-mode web-mode json-mode org-download indent-info orgit edit-indirect pytest py-isort pippel importmagic pip-requirements blacken conda cython-mode company-anaconda anaconda-mode helm cdlatex magic-latex-buffer company-reftex company-auctex auctex-latexmk tex org-mime ox-latex org-edit-latex org-src org-capture org-journal org-projectile org-agenda ob-C ob-latex ob-emacs-lisp ob-python htmlize pyim evil-org toc-org forge evil-magit gist gitignore-mode gitconfig-mode gitattributes-mode git-timemachine gitignore-templates git-messenger git-link diff-hl browse-at-remote dotenv-mode window-purpose eyebrowse persp-mode indent-guide yatemplate yasnippet-snippets writeroom-mode which-key wgrep wakatime-mode vterm volatile-highlights use-package theme-changer terminal-here string-inflection solarized-theme smex shell-pop reveal-in-osx-finder restart-emacs request rainbow-delimiters popwin pdf-tools pandoc-mode org-plus-contrib org-bullets linum-relative link-hint ivy-yasnippet ivy-xref ivy-rich ivy-posframe ivy-hydra hl-todo highlight-parentheses highlight-numbers helpful helm-github-stars google-translate golden-ratio general fuzzy find-by-pinyin-dired fcitx expand-region exec-path-from-shell evil-visualstar evil-textobj-line evil-surround evil-matchit evil-lion evil-indent-plus evil-ediff evil-commentary eval-sexp-fu dumb-jump doom-themes doom-modeline diminish devdocs define-word dash-at-point counsel-projectile counsel-dash company-statistics company-quickhelp company-box clean-aindent-mode centered-cursor-mode cal-china-x benchmark-init auto-package-update aggressive-indent ace-pinyin ace-link)))
+ '(terminal-here-terminal-command (quote ("open" "-a" "iterm.app" ".")) t)
+ '(wakatime-python-bin nil)
+ '(winner-boring-buffers
+   (quote
+    ("*Completions*" "*Compile-Log*" "*inferior-lisp*" "*Fuzzy Completions*" "*Apropos*" "*Help*" "*cvs*" "*Buffer List*" "*Ibuffer*" "*esh command on file*"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(diff-hl-change ((t (:background nil))))
+ '(diff-hl-delete ((t (:background nil))))
+ '(diff-hl-insert ((t (:background nil))))
+ '(mode-line ((t (:height 0.9))))
+ '(mode-line-inactive ((t (:height 0.9))))
+ '(nobreak-space ((t nil)))
+ '(preview-reference-face ((t (:foreground "black")))))
---
+++ lisp/lang-latex.el
@@ -0,0 +1,234 @@
+;;; lang-latex.el -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  Tianshu Wang
+
+;; Author: Tianshu Wang <volekingsg@gmail.com>
+
+(define-jump-handlers latex-mode)
+
+(use-package tex
+  :ensure auctex
+  :config
+  (setq-default TeX-engine 'xetex)
+  (setq latex-build-command "LatexMk"
+        TeX-auto-save t
+        TeX-command-default latex-build-command
+        TeX-master t
+        TeX-parse-self t
+        TeX-save-query nil
+        TeX-syntactic-comment t
+        TeX-source-correlate-start-server t
+        TeX-source-correlate-mode t
+        ;; Don't insert line-break at inline math
+        LaTeX-fill-break-at-separators nil)
+
+  (setq TeX-view-program-list
+        '(("Preview.app" "open -a Preview.app %o")
+          ("Skim" "open -a Skim.app %o")
+          ("displayline" "displayline -b %n %o %b")
+          ("open" "open %o"))
+        TeX-view-program-selection
+        '((output-dvi "open")
+          (output-pdf "displayline")
+          (output-html "open")))
+
+  (add-hook 'doc-view-mode-hook 'auto-revert-mode)
+
+  (custom-set-faces '(preview-reference-face ((t (:foreground "black")))))
+
+  (defun latex/build ()
+    (interactive)
+    (progn
+      (let ((TeX-save-query nil))
+        (TeX-save-document (TeX-master-file)))
+      (TeX-command latex-build-command 'TeX-master-file -1)))
+
+  (defun latex//autofill ()
+    "Check whether the pointer is currently inside one of the
+environments described in `latex-nofill-env' and if so, inhibits
+the automatic filling of the current paragraph."
+    (defvar latex-nofill-env '("equation"
+                               "equation*"
+                               "align"
+                               "align*"
+                               "tabular"
+                               "tikzpicture")
+      "List of environment names in which `auto-fill-mode' will be inhibited.")
+
+    (let ((do-auto-fill t)
+          (current-environment "")
+          (level 0))
+      (while (and do-auto-fill (not (string= current-environment "document")))
+        (setq level (1+ level)
+              current-environment (LaTeX-current-environment level)
+              do-auto-fill (not (member current-environment latex-nofill-env))))
+      (when do-auto-fill
+        (do-auto-fill))))
+
+  (defun latex/auto-fill-mode ()
+    "Toggle auto-fill-mode using the custom auto-fill function."
+    (interactive)
+    (auto-fill-mode)
+    (setq auto-fill-function 'latex//autofill))
+
+  (add-hook 'LaTeX-mode-hook 'latex/auto-fill-mode)
+  (add-hook 'LaTeX-mode-hook 'TeX-fold-mode)
+  (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+  (add-hook 'LaTeX-mode-hook 'TeX-source-correlate-mode)
+  (add-hook 'LaTeX-mode-hook 'TeX-PDF-mode)
+
+  ;; Rebindings for TeX-font
+  (defun font-bold () (interactive) (TeX-font nil ?\C-b))
+  (defun font-medium () (interactive) (TeX-font nil ?\C-m))
+  (defun font-code () (interactive) (TeX-font nil ?\C-t))
+  (defun font-emphasis () (interactive) (TeX-font nil ?\C-e))
+  (defun font-italic () (interactive) (TeX-font nil ?\C-i))
+  (defun font-clear () (interactive) (TeX-font nil ?\C-d))
+  (defun font-calligraphic () (interactive) (TeX-font nil ?\C-a))
+  (defun font-small-caps () (interactive) (TeX-font nil ?\C-c))
+  (defun font-sans-serif () (interactive) (TeX-font nil ?\C-f))
+  (defun font-normal () (interactive) (TeX-font nil ?\C-n))
+  (defun font-serif () (interactive) (TeX-font nil ?\C-r))
+  (defun font-oblique () (interactive) (TeX-font nil ?\C-s))
+  (defun font-upright () (interactive) (TeX-font nil ?\C-u))
+
+  (general-define-key :keymaps 'TeX-mode-map "<H-S-mouse-1>" 'TeX-view)
+  (despot-def :keymaps '(tex-mode-map TeX-mode-map latex-mode-map LaTeX-mode-map)
+    ","             'TeX-command-master
+    "\\"            'TeX-insert-macro                            ;; C-c C-m
+    "-"             'TeX-recenter-output-buffer                  ;; C-c C-l
+    "%"             'TeX-comment-or-uncomment-paragraph          ;; C-c %
+    ";"             'comment-or-uncomment-region                 ;; C-c ; or C-c :
+    ;; TeX-command-run-all runs compile and open the viewer
+    "a"             'TeX-command-run-all                         ;; C-c C-a
+    "b"             'latex/build
+    ;; TeX-doc is a very slow function
+    "h"             '(:ignore t :which-key "help")
+    "hd"            'TeX-doc
+    "k"             'TeX-kill-job                                ;; C-c C-k
+    "l"             'TeX-recenter-output-buffer                  ;; C-c C-l
+    "m"             'TeX-insert-macro                            ;; C-c C-m
+    "n"             'TeX-next-error                              ;; C-c `
+    "N"             'TeX-previous-error                          ;; M-g p
+    "v"             'TeX-view                                    ;; C-c C-v
+    "x"             '(:ignore t :which-key "text/fonts")
+    "xb"            'font-bold
+    "xc"            'font-code
+    "xe"            'font-emphasis
+    "xi"            'font-italic
+    "xr"            'font-clear
+    "xo"            'font-oblique
+    "xf"            '(:ignore t :which-key "fonts")
+    "xfc"           'font-small-caps
+    "xff"           'font-sans-serif
+    "xfr"           'font-serif
+    "z"             '(:ignore t :which-key "fold")
+    "z="            'TeX-fold-math
+    "zb"            'TeX-fold-buffer
+    "zB"            'TeX-fold-clearout-buffer
+    "ze"            'TeX-fold-env
+    "zI"            'TeX-fold-clearout-item
+    "zm"            'TeX-fold-macro
+    "zp"            'TeX-fold-paragraph
+    "zP"            'TeX-fold-clearout-paragraph
+    "zr"            'TeX-fold-region
+    "zR"            'TeX-fold-clearout-region
+    "zz"            'TeX-fold-dwim)
+
+  (despot-def :keymaps '(latex-mode-map LaTeX-mode-map)
+    "*"             'LaTeX-mark-section                  ;; C-c *
+    "."             'LaTeX-mark-environment              ;; C-c .
+    "c"             'LaTeX-close-environment             ;; C-c ]
+    "e"             'LaTeX-environment                   ;; C-c C-e
+    "f"             '(:ignore t :which-key "fill")
+    "fe"            'LaTeX-fill-environment              ;; C-c C-q C-e
+    "fp"            'LaTeX-fill-paragraph                ;; C-c C-q C-p
+    "fr"            'LaTeX-fill-region                   ;; C-c C-q C-r
+    "fs"            'LaTeX-fill-section                  ;; C-c C-q C-s
+    "i"             '(:ignore t :which-key "insert")
+    "ii"            'LaTeX-insert-item                   ;; C-c C-j
+    "p"             '(:ignore t :which-key "preview")
+    "pb"            'preview-buffer
+    "pc"            'preview-clearout
+    "pd"            'preview-document
+    "pe"            'preview-environment
+    "pf"            'preview-cache-preamble
+    "pp"            'preview-at-point
+    "pr"            'preview-region
+    "ps"            'preview-section
+    "s"             'LaTeX-section                       ;; C-c C-s
+    "x"             '(:ignore t :which-key "text/fonts")
+    "xb"            'font-bold
+    "xB"            'font-medium
+    "xc"            'font-code
+    "xe"            'font-emphasis
+    "xf"            '(:ignore t :which-key "fonts")
+    "xfa"           'font-calligraphic
+    "xfc"           'font-small-caps
+    "xff"           'font-sans-serif
+    "xfn"           'font-normal
+    "xfr"           'font-serif
+    "xfu"           'font-upright
+    "xi"            'font-italic
+    "xr"            'font-clear
+    "xo"            'font-oblique))
+
+(use-package reftex
+  :ensure nil
+  :hook (TeX-mode . turn-on-reftex)
+  :config
+  (setq reftex-plug-into-AUCTeX '(nil nil t t t)
+        reftex-use-fonts t)
+
+  (despot-def :keymaps '(latex-mode-map LaTeX-mode-map)
+    "r"     '(:ignore t :which-key "reftex")
+    "rc"    'reftex-citation
+    "rg"    'reftex-grep-document
+    "ri"    'reftex-index-selection-or-word
+    "rI"    'reftex-display-index
+    "r TAB" 'reftex-index
+    "rl"    'reftex-label
+    "rp"    'reftex-index-phrase-selection-or-word
+    "rP"    'reftex-index-visit-phrases-buffer
+    "rr"    'reftex-reference
+    "rs"    'reftex-search-document
+    "rt"    'reftex-toc
+    "rT"    'reftex-toc-recenter
+    "rv"    'reftex-view-crossref))
+
+(use-package cdlatex
+  :hook ((LaTeX-mode . turn-on-cdlatex)
+         (org-mode   . turn-on-org-cdlatex))
+  :config
+  (setq cdlatex-command-alist
+        '(("ct" "Insert \\citet" "\\citet{?}" cdlatex-position-cursor nil t nil)
+          ("cp" "Insert \\citep" "\\citep{?}" cdlatex-position-cursor nil t nil)
+          ("eref" "Insert \\eqref" "\\eqref{eq?}" cdlatex-position-cursor nil t nil)
+          ("fref" "Insert \\ref" "\\ref{fig?}" cdlatex-position-cursor nil t nil)
+          ("sref" "Insert \\ref" "\\ref{sec?}" cdlatex-position-cursor nil t nil))))
+
+(use-package auctex-latexmk
+  :hook (LaTeX-mode . auctex-latexmk-setup)
+  :config (setq auctex-latexmk-inherit-TeX-PDF-mode t))
+
+(use-package company-auctex
+  :after (auctex company)
+  :config (company-auctex-init))
+
+(use-package company-reftex
+  :after company
+  :hook (reftex-mode . (lambda ()
+                         (add-to-list (make-local-variable 'company-backends)
+                                      '(company-reftex-labels company-reftex-citations)))))
+
+(use-package magic-latex-buffer
+  :hook (TeX-update-style . magic-latex-buffer)
+  :config
+  (setq magic-latex-enable-block-highlight t
+        magic-latex-enable-suscript t
+        magic-latex-enable-pretty-symbols t
+        magic-latex-enable-block-align nil
+        magic-latex-enable-inline-image nil))
+
+
+(provide 'lang-latex)
