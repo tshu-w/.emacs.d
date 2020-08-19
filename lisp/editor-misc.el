fb56a3e01d61611d13d975f48753b4a656e55a75
wangtianshu <volekingsg@gmail.com>
2020-08-19 10:10:59 +0800

    add winum for multi windows situation

--- lisp/editor-misc.el
+++ lisp/editor-misc.el
@@ -303,6 +303,73 @@
     "xiu" 'string-inflection-underscore
     "xiU" 'string-inflection-upcase))
 
+(use-package winum
+  :ensure t
+  :hook (after-init . winum-mode)
+  :config
+  (setq winum-auto-assign-0-to-minibuffer nil
+        winum-auto-setup-mode-line t)
+
+  (defun move-buffer-to-window (windownum follow-focus-p)
+    "Moves a buffer to a window. follow-focus-p controls
+whether focus moves to new window (with buffer), or stays on current"
+    (interactive)
+    (let ((b (current-buffer))
+          (w1 (selected-window))
+          (w2 (winum-get-window-by-number windownum)))
+      (unless (eq w1 w2)
+        (set-window-buffer w2 b)
+        (switch-to-prev-buffer)
+        (unrecord-window-buffer w1 b)))
+    (when follow-focus-p (select-window (winum-get-window-by-number windownum))))
+
+  (defun swap-buffers-to-window (windownum follow-focus-p)
+    "Swaps visible buffers between active window and selected window.
+follow-focus-p controls whether focus moves to new window (with buffer), or
+stays on current"
+    (interactive)
+    (let* ((b1 (current-buffer))
+           (w1 (selected-window))
+           (w2 (winum-get-window-by-number windownum))
+           (b2 (window-buffer w2)))
+      (unless (eq w1 w2)
+        (set-window-buffer w1 b2)
+        (set-window-buffer w2 b1)
+        (unrecord-window-buffer w1 b1)
+        (unrecord-window-buffer w2 b2)))
+    (when follow-focus-p (winum-select-window-by-number windownum)))
+
+  (dotimes (i 9)
+    (let ((n (+ i 1)))
+      (eval `(defun ,(intern (format "buffer-to-window-%s" n)) (&optional arg)
+               ,(format "Move buffer to the window with number %i." n)
+               (interactive "P")
+               (if arg
+                   (move-buffer-to-window ,n t)
+                 (swap-buffers-to-window ,n t))))))
+  :general
+  (tyrant-def
+    "`" '(winum-select-window-by-number :which-key "select window by number")
+    "0" '(winum-select-window-0-or-10   :which-key "select window 0 or 10")
+    "1" '(winum-select-window-1         :which-key ("1\.\.9" . "select window 1..9"))
+    "2" '(winum-select-window-2         :which-key t)
+    "3" '(winum-select-window-3         :which-key t)
+    "4" '(winum-select-window-4         :which-key t)
+    "5" '(winum-select-window-5         :which-key t)
+    "6" '(winum-select-window-6         :which-key t)
+    "7" '(winum-select-window-7         :which-key t)
+    "8" '(winum-select-window-8         :which-key t)
+    "9" '(winum-select-window-9         :which-key t)
+    "b1" '(buffer-to-window-1           :which-key ("1\.\.9" . "Move buffer to window 1..9"))
+    "b2" '(buffer-to-window-2           :which-key t)
+    "b3" '(buffer-to-window-3           :which-key t)
+    "b4" '(buffer-to-window-4           :which-key t)
+    "b5" '(buffer-to-window-5           :which-key t)
+    "b6" '(buffer-to-window-6           :which-key t)
+    "b7" '(buffer-to-window-7           :which-key t)
+    "b8" '(buffer-to-window-8           :which-key t)
+    "b9" '(buffer-to-window-9           :which-key t)))
+
 (use-package wakatime-mode
   :ensure t
   :hook (prog-mode . wakatime-mode)
