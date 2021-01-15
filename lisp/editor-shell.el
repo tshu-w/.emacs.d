7c3101e6e2f90898a7d452ce9d8dd087e769afb9
Tianshu Wang <volekingsg@gmail.com>
2021-01-15 13:39:10 +0800

    Use eshell instead of vterm

--- lisp/editor-shell.el
+++ lisp/editor-shell.el
@@ -8,41 +8,13 @@
 
 ;;; Code:
 
-(use-package vterm
-  :ensure t
-  :commands (vterm vterm-other-window)
-  :config
-  (setq vterm-buffer-name-string "vterm %s"
-        vterm-clear-scrollback-when-clearing t
-        vterm-kill-buffer-on-exit t)
-
-  (add-hook 'vterm-mode-hook (lambda () (setq-local global-hl-line-mode nil)))
-
-  (with-eval-after-load 'writeroom-mode
-    (add-to-list 'writeroom-major-modes-exceptions 'vterm-mode))
-
-  (with-eval-after-load 'centered-cursor-mode
-    (add-hook 'vterm-mode-hook
-              (lambda ()
-                (add-hook 'after-change-major-mode-hook
-                          (lambda () (centered-cursor-mode 0))
-                          :append
-                          :local))))
-
-  (evil-set-initial-state 'vterm-mode 'emacs)
-  (general-def 'emacs vterm-mode-map
-    "C-y"      'vterm-yank
-    "C-c C-d"  'vterm-send-C-d
-    "C-c C-z"  'vterm-send-C-z))
-
 (use-package shell-pop
   :ensure t
   :init
   (setq shell-pop-window-position 'bottom
         shell-pop-window-size     50
         shell-pop-term-shell      shell-file-name
-        shell-pop-shell-type      '("vterm" "*vterm*"
-                                    (lambda nil (vterm)))
+        shell-pop-shell-type      '("eshell" "*eshell*" (lambda () (eshell)))
         shell-pop-full-span       t)
   :general
   (tyrant-def "'" '(shell-pop :which-key "open shell")))
