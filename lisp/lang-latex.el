66e80a39074e8a0ad4ca3739116612facf39812e
Tianshu Wang <volekingsg@gmail.com>
2021-01-15 10:40:02 +0800

    Fix latex config errors and remove useless packages

--- lisp/lang-latex.el
+++ lisp/lang-latex.el
@@ -11,37 +11,57 @@
 (use-package tex
   :ensure auctex
   :defer t
+  :mode ("\\.[tT]e[xX]\\'" . TeX-tex-mode)
   :custom-face (preview-reference-face ((t (:foreground "black"))))
   :config
   (setq-default TeX-engine 'xetex)
 
-  (setq latex-build-command "LatexMk"
-        TeX-auto-save t
-        TeX-command-default latex-build-command
+  (setq TeX-auto-save t
+        TeX-command-default "LatexMk"
         TeX-master t
         TeX-parse-self t
         TeX-save-query nil
         TeX-source-correlate-start-server t
-        ;; Don't insert line-break at inline math
-        LaTeX-fill-break-at-separators nil)
-
-  (setq TeX-view-program-list '(("Preview.app" "open -a Preview.app %o")
+        TeX-view-program-list '(("Preview.app" "open -a Preview.app %o")
                                 ("Skim" "open -a Skim.app %o")
                                 ("displayline" "displayline -b %n %o %b")
                                 ("open" "open %o"))
         TeX-view-program-selection '((output-dvi "open")
                                      (output-pdf "displayline")
-                                     (output-html "open")))
+                                     (output-html "open"))
+        ;; Don't insert line-break at inline math
+        LaTeX-fill-break-at-separators nil)
 
-  (defun TeX-build ()
+  (defun latex-autofill ()
+    "Check whether the pointer is currently inside one of the
+environments described in `latex-nofill-env' and if so, inhibits
+the automatic filling of the current paragraph."
+    (let ((do-auto-fill t)
+          (current-environment "")
+          (level 0))
+      (while (and do-auto-fill (not (string= current-environment "document")))
+        (setq level (1+ level)
+              current-environment (LaTeX-current-environment level)
+              do-auto-fill (not (member current-environment latex-nofill-env))))
+      (when do-auto-fill
+        (do-auto-fill))))
+
+  (defun latex-auto-fill-mode ()
+    "Toggle auto-fill-mode using the custom auto-fill function."
     (interactive)
-    (TeX-save-document (TeX-master-file))
-    (TeX-command latex-build-command 'TeX-master-file -1))
+    (auto-fill-mode)
+    (setq auto-fill-function #'latex-autofill))
 
+  (add-hook 'LaTeX-mode-hook 'latex-auto-fill-mode)
   (add-hook 'LaTeX-mode-hook 'TeX-fold-mode)
   (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
   (add-hook 'LaTeX-mode-hook 'TeX-source-correlate-mode)
   (add-hook 'LaTeX-mode-hook 'TeX-PDF-mode)
+
+  (defun TeX-build ()
+    (interactive)
+    (TeX-save-document (TeX-master-file))
+    (TeX-command TeX-command-default 'TeX-master-file -1))
 
   ;; Rebindings for TeX-font
   (defun font-bold () (interactive) (TeX-font nil ?\C-b))
@@ -59,7 +79,9 @@
   (defun font-upright () (interactive) (TeX-font nil ?\C-u))
 
   (general-def TeX-mode-map "<H-S-mouse-1>" 'TeX-view)
-  (despot-def :keymaps '(tex-mode-map TeX-mode-map latex-mode-map LaTeX-mode-map)
+
+  (despot-def (TeX-mode-map LaTeX-mode-map)
+    :major-modes '(tex-mode latex-mode)
     ","             'TeX-command-master
     "\\"            'TeX-insert-macro                          ;; C-c C-m
     "-"             'TeX-recenter-output-buffer                ;; C-c C-l
@@ -101,7 +123,8 @@
     "zR"            'TeX-fold-clearout-region
     "zz"            'TeX-fold-dwim)
 
-  (despot-def :keymaps '(latex-mode-map LaTeX-mode-map)
+  (despot-def LaTeX-mode-map
+    :major-modes '(latex-mode)
     "*"                'LaTeX-mark-section                  ;; C-c *
     "."                'LaTeX-mark-environment              ;; C-c .
     "c"                'LaTeX-close-environment             ;; C-c ]
@@ -124,16 +147,10 @@
     "ps"               'preview-section
     "s"                'LaTeX-section                       ;; C-c C-s
     "x"                '(:ignore t :which-key "text/fonts")
-    "xb"               'font-bold
     "xB"               'font-medium
-    "xc"               'font-code
-    "xe"               'font-emphasis
     "xf"               '(:ignore t :which-key "fonts")
     "xfa"              'font-calligraphic
-    "xfc"              'font-small-caps
-    "xff"              'font-sans-serif
     "xfn"              'font-normal
-    "xfr"              'font-serif
     "xfu"              'font-upright
     "xi"               'font-italic
     "xr"               'font-clear
@@ -145,7 +162,8 @@
   (setq reftex-plug-into-AUCTeX '(nil nil t t t)
         reftex-use-fonts t)
 
-  (despot-def :keymaps '(latex-mode-map LaTeX-mode-map)
+  (despot-def (TeX-mode-map LaTeX-mode-map)
+    :major-modes '(tex-mode latex-mode)
     "r"     '(:ignore t :which-key "reftex")
     "rc"    'reftex-citation
     "rg"    'reftex-grep-document
@@ -174,7 +192,6 @@
           ("sref" "Insert \\ref" "\\ref{sec?}" cdlatex-position-cursor nil t nil))
         cdlatex-make-sub-superscript-roman-if-pressed-twice t))
 
-
 (use-package auctex-latexmk
   :ensure t
   :after tex
@@ -185,7 +202,13 @@
 (use-package company-auctex
   :ensure t
   :after tex
-  :config (company-auctex-init))
+  :hook (TeX-mode . (lambda ()
+                      (add-to-list (make-local-variable 'company-backends) 'company-auctex-labels)
+                      (add-to-list (make-local-variable 'company-backends) 'company-auctex-bibs)
+                      (add-to-list (make-local-variable 'company-backends)
+                                   '(company-auctex-macros
+                                     company-auctex-symbols
+                                     company-auctex-environments)))))
 
 (use-package company-reftex
   :ensure t
@@ -194,43 +217,6 @@
                       (add-to-list
                        (make-local-variable 'company-backends)
                        '(company-reftex-labels company-reftex-citations)))))
-
-(use-package magic-latex-buffer
-  :ensure t
-  :hook (TeX-mode . magic-latex-buffer)
-  :config
-  (setq magic-latex-enable-block-highlight nil
-        magic-latex-enable-suscript        t
-        magic-latex-enable-pretty-symbols  nil
-        magic-latex-enable-block-align     nil
-        magic-latex-enable-inline-image    nil
-        magic-latex-enable-minibuffer-echo t))
-
-(use-package ebib
-  :disabled t
-  :ensure t
-  :config
-  (setq bibtex-dialect 'biblatex
-        ebib-default-directory "~/Documents/Zotero"
-        ebib-bib-search-dirs '("~/Documents/Zotero")
-        ebib-preload-bib-files '("references.bib")
-        ebib-notes-directory "~/Documents/Org/notes/papers/"
-        ebib-notes-template "#+TITLE: %T\n#+roam_key: %C\n\n"
-        ebib-reading-list-file "~/Documents/Org/reading_list.org"
-        ebib-bibtex-dialect 'biblatex
-        ebib-index-columns '(("Entry Key" 20 t)
-                             ("Author/Editor" 40 t)
-                             ("Year" 6 t)
-                             ("Title" 50 t))
-        ebib-index-default-sort '("timestamp" . descend)
-        ebib-file-associations '(("pdf" . "open"))
-        ebib-filename-separator ";"
-        ebib-uniquify-keys t
-        ebib-use-timestamp t)
-
-  (general-def ebib-index-mode-map "/" 'ebib-jump-to-entry)
-  :general
-  (tyrant-def "ae" 'ebib))
 
 (use-package ivy-bibtex
   :ensure t
@@ -244,6 +230,7 @@
 
 (use-package xenops
   :ensure t
+  :disabled t
   :hook (LaTeX-mode . xenops-mode)
   :config
   (setq xenops-cache-directory (no-littering-expand-var-file-name "xenops")))
