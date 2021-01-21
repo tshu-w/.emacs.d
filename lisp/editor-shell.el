36ff4ff5819ce23a379f839bf7fb5fa429352ee4
Tianshu Wang <volekingsg@gmail.com>
2021-01-21 22:23:53 +0800

    Use popwin for shell-pop and remove eshell-prompt-extras

--- lisp/editor-shell.el
+++ lisp/editor-shell.el
@@ -8,14 +8,6 @@
 
 ;;; Code:
 
-(use-package shell-pop
-  :ensure t
-  :init
-  (setq shell-pop-window-position 'bottom
-        shell-pop-window-size     50
-        shell-pop-term-shell      shell-file-name
-        shell-pop-shell-type      '("eshell" "*eshell*" (lambda () (eshell)))
-        shell-pop-full-span       t)
 (use-package comint
   :defer t
   :config
@@ -47,6 +39,13 @@
     (mapc (lambda (x) (add-to-list 'eshell-visual-commands x))
           '("el" "elinks" "htop" "less" "ssh" "tmux" "top")))
 
+  (defun projectile-eshell ()
+    "Open a term buffer at projectile project root."
+    (interactive)
+    (when (get-buffer "*eshell*") (kill-buffer "*eshell*"))
+    (let ((default-directory (projectile-project-root)))
+      (call-interactively 'eshell)))
+
   (defun eshell/clear ()
     (interactive)
     (let ((inhibit-read-only t))
@@ -67,30 +66,9 @@
     "C-r"   'counsel-esh-history
     "M-."   'eshell-yank-last-arg)
   :general
-  (tyrant-def "'" '(shell-pop :which-key "open shell")))
-(use-package eshell-prompt-extras
-  :ensure t
-  :after eshell
-  :config
-  (setq eshell-highlight-prompt nil
-        eshell-prompt-function 'epe-theme-lambda)
-
-  (defun protect-eshell-prompt ()
-    "Protect Eshell's prompt like Comint's prompts.
-
-E.g. `evil-change-whole-line' won't wipe the prompt. This
-is achieved by adding the relevant text properties."
-    (let ((inhibit-field-text-motion t))
-      (add-text-properties
-       (point-at-bol)
-       (point)
-       '(rear-nonsticky t
-                        inhibit-line-move-field-capture t
-                        field output
-                        read-only t
-                        front-sticky (field inhibit-line-move-field-capture)))))
-
-  (add-hook 'eshell-after-prompt-hook #'protect-eshell-prompt))
+  (tyrant-def
+    "'"  'eshell
+    "p'" 'projectile-eshell))
 
 (use-package term
   :defer t
