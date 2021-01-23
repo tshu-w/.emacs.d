c1e413d69c26e6b54d7133e2570aedd355646151
Tianshu Wang <volekingsg@gmail.com>
2021-01-23 15:23:41 +0800

    Remove pyim as evil-pinyin provides a similar function

--- lisp/core-keybinds.el
+++ lisp/core-keybinds.el
@@ -463,6 +463,7 @@
 (use-package evil-pinyin
   :ensure t
   :hook (after-init . global-evil-pinyin-mode)
+  :commands (evil-pinyin--build-regexp)
   :init
   (setq evil-pinyin-scheme 'simplified-xiaohe-all))
 
