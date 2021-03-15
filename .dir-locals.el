91c5a3a56c7633f108a632b055ffa64bb195ba6f
Tianshu Wang <wang@tianshu.me>
2021-03-15 20:20:39 +0800

    Add directory configuration

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((emacs-lisp-mode . ((byte-compile-warnings . (not free-vars unresolved))
+                     (flycheck-disabled-checkers . (emacs-lisp-checkdoc)))))
