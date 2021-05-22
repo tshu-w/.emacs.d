2151f825f0908ff637393f9e9eeaf8a5873fbe24
Tianshu Wang <wang@tianshu.me>
2021-05-22 16:33:01 +0800

    Move xterm-color to editor-ui.el

--- lisp/editor-shell.el
+++ lisp/editor-shell.el
@@ -84,17 +84,6 @@
     "\""   'terminal-here-launch
     "p \"" 'terminal-here-project-launch))
 
-(use-package xterm-color
-  :ensure t
-  :after eshell
-  :config
-  (add-hook 'eshell-before-prompt-hook
-            (lambda () (setq xterm-color-preserve-properties t)))
-
-  (add-to-list 'eshell-preoutput-filter-functions 'xterm-color-filter)
-  (setq eshell-output-filter-functions (remove 'eshell-handle-ansi-color eshell-output-filter-functions))
-  (setenv "TERM" "xterm-256color"))
-
 
 (provide 'editor-shell)
 ;;; editor-shell.el ends here
