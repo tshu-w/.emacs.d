f80950e1b02e2229bc89c5671631c53a40fde906
Tianshu Wang <wang@tianshu.me>
2021-07-02 08:44:48 +0800

    Hide the internal-border

--- early-init.el
+++ early-init.el
@@ -39,6 +39,7 @@
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
+(push '(internal-border-width . 0) default-frame-alist)
 
 (when (featurep 'ns)
   (push '(ns-transparent-titlebar . t) default-frame-alist))
