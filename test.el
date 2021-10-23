ae771f62aca1acf01885bd4f7fd37ab69a12a1ae
Tianshu Wang <wang@tianshu.me>
2021-10-23 17:30:56 +0800

    Update test.el

--- test.el
+++ test.el
@@ -1,26 +1,4 @@
-;;; emacs -nw -Q -l ~/.emacs.d/test.el
+;;; emacs -Q -l ~/.config/emacs/test.el
 
-(setq package-archives '(("melpa"        . "https://melpa.org/packages/")
-                         ("melpa-stable" . "https://stable.melpa.org/packages/")
-                         ("org"          . "https://orgmode.org/elpa/")
-                         ("gnu"          . "https://elpa.gnu.org/packages/"))
-      package-user-dir (concat user-emacs-directory "elpa/"
-                               (format "%d%s%d"
-                                       emacs-major-version
-                                       version-separator
-                                       emacs-minor-version)))
+(package-initialize)
 
-;; initialize packages
-(unless (bound-and-true-p package--initialized) ; To avoid warnings in 27
-  (setq package-enable-at-startup nil)          ; To prevent initializing twice
-  (package-initialize))
-
-;; setup `use-package'
-(unless (package-installed-p 'use-package)
-  (when (not package-archive-contents)
-    (package-refresh-contents))
-  (package-install 'use-package))
-
-(require 'use-package)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
