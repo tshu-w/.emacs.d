1a72ead8eb9b06982c67fb517085c1611cda3f65
Tianshu Wang <wang@tianshu.me>
2021-10-26 22:06:37 +0800

    Improve format

--- lisp/core-config.el
+++ lisp/core-config.el
@@ -38,9 +38,9 @@
 (defun set-monospaced-font (english chinese english-size chinese-size)
   "Set the monospaced font size when mixed CHINESE and ENGLISH words."
   (set-face-attribute 'default nil :font
-                      (format   "%s:pixelsize=%d"  english english-size))
+                      (format "%s:pixelsize=%d" english english-size))
   (dolist (charset '(kana han cjk-misc bopomofo))
-    (set-fontset-font (frame-parameter nil 'font) charset
+    (set-fontset-font t charset
                       (font-spec :family chinese :size chinese-size))))
 (when (display-graphic-p)
   (set-monospaced-font "Source Code Pro" "PingFang SC" 14 16))
