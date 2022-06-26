4600d95686360b610c94fcffd5df38d7077e2e20
Tianshu Wang <wang@tianshu.me>
2022-06-26 12:56:31 +0800

    Replace vertical bar with hyphens

--- lisp/lang-org.el
+++ lisp/lang-org.el
@@ -278,7 +278,7 @@
     (defun org-capture-goto-link ()
       (let ((file (nth 1 (org-capture-get :target)))
             (headline (replace-regexp-in-string
-                       "|\s*" ""
+                       "|" "-"
                        (plist-get org-store-link-plist :description)))
             (link (plist-get org-store-link-plist :link)))
         (org-capture-put :target (list 'file+headline file headline))
