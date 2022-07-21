ceb20b82f8267cc71fa5c0c047b667576e221567
Tianshu Wang <wang@tianshu.me>
2022-07-21 12:21:15 +0800

    Fix straight link

--- lisp/core-packages.el
+++ lisp/core-packages.el
@@ -30,7 +30,7 @@
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
         (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
          'silent 'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)))
