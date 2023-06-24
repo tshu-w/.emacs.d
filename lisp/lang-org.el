b24655f4c2bc75f174237786250f1204351e0d66
Tianshu Wang <wang@tianshu.me>
2023-06-24 14:00:35 +0800

    Fix keybinds for org-capture

--- lisp/lang-org.el
+++ lisp/lang-org.el
@@ -603,7 +603,7 @@
   (with-eval-after-load 'org-capture
     (add-hook 'org-capture-mode-hook #'evil-insert-state)
     (add-hook 'org-capture-after-finalize-hook #'evil-normal-state)
-    (general-def org-capture-mode-map
+    (general-def 'normal org-capture-mode-map
       "ZZ" 'org-capture-finalize
       "ZQ" 'org-capture-kill
       "ZR" 'org-capture-refile)))
