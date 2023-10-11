449766317f8347e9c46badb3487b04f0f260e863
Tianshu Wang <wang@tianshu.me>
2023-10-11 12:33:08 +0800

    Remove the confluence backend

--- lisp/lang-org.el
+++ lisp/lang-org.el
@@ -51,7 +51,7 @@
         org-use-property-inheritance t
         org-use-sub-superscripts '{}
         org-export-with-sub-superscripts '{}
-        org-export-backends '(beamer html icalendar latex md confluence)
+        org-export-backends '(beamer html icalendar latex md)
         org-yank-adjusted-subtrees t)
 
   (add-hook 'org-mode-hook
