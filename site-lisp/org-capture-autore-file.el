78582f50e8be6987872dfc3c42b318b46d66cfe3
Tianshu Wang <wang@tianshu.me>
2024-06-27 16:15:04 +0800

    Bump copyright year to 2024

--- site-lisp/org-capture-autore-file.el
+++ site-lisp/org-capture-autore-file.el
@@ -1,8 +1,23 @@
 ;;; org-capture-autorefile.el --- -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2022-2023  Tianshu Wang
+;; Copyright (C) 2022-2024  Tianshu Wang
 
 ;; Author: Tianshu Wang <wang@tianshu.me>
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
