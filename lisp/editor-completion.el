709b09cafea4dda792893135274f5d7fa492c741
Tianshu Wang <wang@tianshu.me>
2024-06-27 16:15:04 +0800

    Bump copyright year to 2024

--- lisp/editor-completion.el
+++ lisp/editor-completion.el
@@ -1,6 +1,6 @@
 ;;; editor-completion.el --- -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2020-2023  Tianshu Wang
+;; Copyright (C) 2020-2024  Tianshu Wang
 
 ;; Author: Tianshu Wang <wang@tianshu.me>
 
