a885fd4614d2a783102ef24877994bb9c6bb951b
Tianshu Wang <wang@tianshu.me>
2024-06-27 16:15:04 +0800

    Bump copyright year to 2024

--- lisp/core-packages.el
+++ lisp/core-packages.el
@@ -1,6 +1,6 @@
 ;;; core-packages.el --- -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2020-2023  Tianshu Wang
+;; Copyright (C) 2020-2024  Tianshu Wang
 
 ;; Author: Tianshu Wang <wang@tianshu.me>
 
