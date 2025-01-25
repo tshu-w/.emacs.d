06abbde0c51617cc6574ef7dc23a767566ee01a5
Tianshu Wang <wang@tianshu.me>
2025-01-25 17:28:37 +0800

    Refactor recentf configs

--- lisp/core-config.el
+++ lisp/core-config.el
@@ -247,11 +247,10 @@
   :hook (after-init . recentf-mode)
   :config
   (setq recentf-auto-cleanup 'never
-        recentf-filename-handlers '(abbreviate-file-name)
         recentf-max-saved-items 100
         recentf-initialize-file-name-history nil)
 
-  (add-to-list 'recentf-exclude `(recentf-expand-file-name ,(straight--emacs-dir "straight"))))
+  (add-to-list 'recentf-exclude (recentf-expand-file-name (straight--emacs-dir "straight"))))
 
 (use-package repeat
   :hook (after-init . repeat-mode)
