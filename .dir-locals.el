23a44440553cb363864427576e0687250be94405
Tianshu Wang <wang@tianshu.me>
2025-02-28 13:22:43 +0800

    Hack TeX-output-extension for Latexmk+xetex+TeX-PDF-Mode

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,6 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((emacs-lisp-mode . ((flymake-diagnostic-functions . nil))))
+((emacs-lisp-mode . ((flymake-diagnostic-functions . nil)
+                     (bug-reference-url-format . "https://debbugs.gnu.org/cgi/bugreport.cgi?bug=%s")
+                     (bug-reference-bug-regexp . "\\(\\b\\(?:[Bb]ug ?#?\\)\\([0-9]+\\(?:#[0-9]+\\)?\\)\\)\\>"))))
