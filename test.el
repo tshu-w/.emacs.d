a1ea6b110e22968a2071154bc8101ad8e5e083a1
Tianshu Wang <wang@tianshu.me>
2025-10-04 14:12:23 +0800

    Enable lexical-binding

--- test.el
+++ test.el
@@ -1,4 +1,4 @@
-;;; emacs -Q -l ~/.config/emacs/test.el
+;;; emacs -Q -l ~/.config/emacs/test.el  -*- lexical-binding: t; -*-
 
 (setq package-archives '(("melpa"        . "https://melpa.org/packages/")
                          ("gnu"          . "https://elpa.gnu.org/packages/")
